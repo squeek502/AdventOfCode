2ae7094bd5129e4bc9468d7b6ad6111f181edf69
Ryan Liptak <squeek502@hotmail.com>
2017-11-30 04:17:52 -0800

    Day 2 part 2

--- 02/init.lua
+++ 02/init.lua
@@ -9,14 +9,21 @@ local get = function(path)
 end
 local input = get('input.txt')
 
-local needed = 0
+local wrappingNeeded = 0
+local ribbonNeeded = 0
 
 for _,line in ipairs(input) do
   local l, w, h = line:match("(%d+)x(%d+)x(%d+)")
   l, w, h = tonumber(l), tonumber(w), tonumber(h)
+
   local surfaceArea = 2*l*w + 2*w*h + 2*h*l
   local slack = math.min(l*w, w*h, h*l)
-  needed = needed + surfaceArea + slack
+  wrappingNeeded = wrappingNeeded + surfaceArea + slack
+
+  local smallestSidePerimeter = math.min(l+w, w+h, h+l)*2
+  local volume = l*w*h
+  ribbonNeeded = ribbonNeeded + smallestSidePerimeter + volume
 end
 
-print("Total sqft needed", needed)
+print("Total wrapping needed (sqft)", wrappingNeeded)
+print("Total ribbon needed (ft)", ribbonNeeded)
