d6313dea0a545e3b41233d9d6aaf1a117a8c144a
Ryan Liptak <squeek502@hotmail.com>
2017-12-01 23:10:40 -0800

    Better 2015 day 6 with both parts in 1 file

--- 2015/06/init.lua
+++ 2015/06/init.lua
@@ -11,20 +11,16 @@ local input = get('input.txt')
 
 local w, h = 1000, 1000
 
-local grid = {}
-
-for i=1,w do
-  grid[i] = {}
-  for j=1,h do
-    grid[i][j] = 0
+local function makeGrid(w, h, defaultValue)
+  local grid = {}
+  for i=0,w-1 do
+    grid[i] = {}
+    for j=0,h-1 do
+      grid[i][j] = defaultValue
+    end
   end
+  return grid
 end
-
-local instructions = {
-  ["turn on"] = function(v) return v+1 end,
-  ["toggle"] = function(v) return v+2 end,
-  ["turn off"] = function(v) return math.max(0, v-1) end,
-}
 
 local function iteratorCollect(iterator)
   local t = {}
@@ -34,22 +30,52 @@ local function iteratorCollect(iterator)
   return t
 end
 
-for _, line in ipairs(input) do
-  local coords = iteratorCollect(line:gmatch('%d+'))
-  local x1, y1, x2, y2 = unpack(coords)
-  local instruction = line:match('^[^%d]+'):sub(1,-2)
-  for x=x1, x2 do
-    for y=y1, y2 do
-      grid[x][y] = instructions[instruction](grid[x][y])
+local function run(grid, instructions, eval)
+  for _, line in ipairs(input) do
+    local coords = iteratorCollect(line:gmatch('%d+'))
+    local x1, y1, x2, y2 = unpack(coords)
+    local instruction = line:match('^[^%d]+'):sub(1,-2)
+    for x=x1, x2 do
+      for y=y1, y2 do
+        grid[x][y] = instructions[instruction](grid[x][y])
+      end
     end
   end
-end
 
-local brightness = 0
-for _, row in ipairs(grid) do
-  for _, v in ipairs(row) do
-    brightness = brightness + v
+  local total = 0
+  for x=0,w-1 do
+    for y=0,h-1 do
+      total = eval(total, grid[x][y])
+    end
   end
+
+  return total
 end
 
+-- part 1
+local grid = makeGrid(w, h, false)
+local instructions = {
+  ["turn on"] = function(v) return true end,
+  ["toggle"] = function(v) return not v end,
+  ["turn off"] = function(v) return false end,
+}
+local eval = function(t, v)
+  return v and t+1 or t
+end
+
+local lit = run(grid, instructions, eval)
+print("Total lit", lit)
+
+-- part 2
+grid = makeGrid(w, h, 0)
+instructions = {
+  ["turn on"] = function(v) return v+1 end,
+  ["toggle"] = function(v) return v+2 end,
+  ["turn off"] = function(v) return math.max(0, v-1) end,
+}
+eval = function(t, v)
+  return t+v
+end
+
+local brightness = run(grid, instructions, eval)
 print("Total brightness", brightness)
