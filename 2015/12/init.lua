10d75d484fb911671765e167d7f1e7ebf3efe8cc
Ryan Liptak <squeek502@hotmail.com>
2017-12-02 09:02:37 -0800

    2015 Day 12 part 2

--- 2015/12/init.lua
+++ 2015/12/init.lua
@@ -12,3 +12,51 @@ for number in input:gmatch("[%d%-]+") do
   sum = sum + number
 end
 print(sum)
+
+local patternEscapes = {
+  ["^"] = "%^", ["$"] = "%$", ["("] = "%(", [")"] = "%)",
+  ["%"] = "%%", ["."] = "%.", ["["] = "%[", ["]"] = "%]",
+  ["*"] = "%*", ["+"] = "%+", ["-"] = "%-", ["?"] = "%?",
+  ["\0"] = "%z",
+}
+local function escapePattern(s)
+  return (s:gsub(".", patternEscapes))
+end
+
+local function gsubPlain(haystack, needle, repl)
+  return haystack:gsub(escapePattern(needle), repl)
+end
+
+local function getInners(str)
+  str = str:sub(2,-2)
+  local inners = {}
+  for inner in str:gmatch("%b{}") do
+    table.insert(inners, inner)
+  end
+  return inners
+end
+
+local function stripRed(str)
+  str = str:gsub("%b{}", function(obj)
+    local inners = getInners(obj)
+    for _, inner in ipairs(inners) do
+      local strippedInner = stripRed(inner)
+      obj = gsubPlain(obj, inner, strippedInner)
+    end
+    if obj:match(":\"red\"") then
+      return ""
+    else
+      return obj
+    end
+  end)
+  return str
+end
+
+input = stripRed(input)
+
+sum = 0
+for number in input:gmatch("[%d%-]+") do
+  number = tonumber(number)
+  sum = sum + number
+end
+print(sum)
