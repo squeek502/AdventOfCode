5b38c681b1f7c13d3791b513e8921a16a4d60d2c
Ryan Liptak <squeek502@hotmail.com>
2017-12-03 05:15:26 -0800

    2015 Day 18 part 1

---
+++ 2015/18/init.lua
@@ -0,0 +1,71 @@
+local get = function(path)
+  local lines = {}
+  for line in io.lines(path) do
+    table.insert(lines, line)
+  end
+  return lines
+end
+local input = get('input.txt')
+
+local grid = {}
+
+for x, line in ipairs(input) do
+  grid[x] = {}
+  for y=1,#line do
+    local c = line:sub(y,y)
+    grid[x][y] = c == '#'
+  end
+end
+
+local neighborOffsets = {
+  {1,0}, {1,1}, {0,1}, {-1,1}, {-1,0}, {-1,-1}, {0,-1}, {1,-1}
+}
+
+local function getLitNeighbors(x, y)
+  local count = 0
+  for _, offset in ipairs(neighborOffsets) do
+    local dx, dy = unpack(offset)
+    if grid[x+dx] and grid[x+dx][y+dy] == true then
+      count = count + 1
+    end
+  end
+  return count
+end
+
+local function step()
+  local toggle = {}
+  for x=1,#grid do
+    for y=1,#grid[x] do
+      local on = grid[x][y]
+      local neighbors = getLitNeighbors(x, y)
+      if on and neighbors ~= 2 and neighbors ~= 3 then
+        table.insert(toggle, {x,y})
+      elseif not on and neighbors == 3 then
+        table.insert(toggle, {x,y})
+      end
+    end
+  end
+
+  for _, coord in ipairs(toggle) do
+    local x, y = unpack(coord)
+    grid[x][y] = not grid[x][y]
+  end
+end
+
+local function countLit()
+  local lit = 0
+  for x=1,#grid do
+    for y=1,#grid[x] do
+      if grid[x][y] then
+        lit = lit + 1
+      end
+    end
+  end
+  return lit
+end
+
+for i=1,100 do
+  step()
+end
+
+print(countLit())
