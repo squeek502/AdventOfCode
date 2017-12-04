8613334bda0c064e99cb2dfaa374c9583514b39a
Ryan Liptak <squeek502@hotmail.com>
2017-12-04 03:43:29 -0800

    2015 Day 19 part 2

--- 2015/19/init.lua
+++ 2015/19/init.lua
@@ -37,3 +37,62 @@ end
 
 local count = getDistinctMolecules(molecule, replacements)
 print(count)
+
+-- Part 2
+local reverseReplacements = {}
+local origins = {}
+for k,v in pairs(replacements) do
+  for _,rep in ipairs(v) do
+    if k ~= "e" then
+      reverseReplacements[rep] = k
+    else
+      origins[rep] = k
+    end
+  end
+end
+
+-- sorting by the delta here seems to lead to a quick solve by happenstance (?)
+-- using the same solve algorithm but sorting by replacement length leads to
+-- a really long solve time
+local sortedByDelta = {}
+for k in pairs(reverseReplacements) do
+  table.insert(sortedByDelta,k)
+end
+table.sort(sortedByDelta, function(a,b)
+  local da = #a - #reverseReplacements[a]
+  local db = #b - #reverseReplacements[b]
+  return da > db
+end)
+
+local function makeMolecule(target)
+  local seen = {}
+  local make
+
+  make = function(target, steps)
+    if steps == nil then steps = 0 end
+    seen[target] = true
+    steps = steps+1
+
+    if origins[target] then
+      return steps
+    end
+
+    for _, k in ipairs(sortedByDelta) do
+      local rep = reverseReplacements[k]
+      for i=1,#target do
+        local new = tryReplace(target, i, k, rep)
+        if new and not seen[new] then
+          local total = make(new, steps)
+          if total then
+            return total
+          end
+        end
+      end
+    end
+  end
+
+  return make(target)
+end
+
+local steps = makeMolecule(molecule)
+print(steps)
