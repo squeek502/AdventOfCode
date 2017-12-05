d3a34e957b9b52c0d9a941332033b56581babfeb
Ryan Liptak <squeek502@hotmail.com>
2017-12-04 21:10:00 -0800

    2017 Day 5 part 2

--- 2017/05/init.lua
+++ 2017/05/init.lua
@@ -15,3 +15,19 @@ while cur <= #instructions do
 end
 
 print(steps)
+
+instructions = {}
+for _, line in ipairs(input) do
+  local jmp = tonumber(line)
+  table.insert(instructions, jmp)
+end
+
+cur, steps = 1, 0
+while cur <= #instructions do
+  local jmp = instructions[cur]
+  instructions[cur] = jmp >= 3 and jmp-1 or jmp+1
+  cur = cur + jmp
+  steps = steps+1
+end
+
+print(steps)
