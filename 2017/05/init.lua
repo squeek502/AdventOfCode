a5c2785175d5d791db6b1b41c89f250fd2f14496
Ryan Liptak <squeek502@hotmail.com>
2017-12-04 21:07:09 -0800

    2017 Day 5 part 1

---
+++ 2017/05/init.lua
@@ -0,0 +1,17 @@
+local input = (function(f) local t={}; for l in assert(io.lines(f)) do t[#t+1] = l end; return t end)('input.txt')
+
+local instructions = {}
+for _, line in ipairs(input) do
+  local jmp = tonumber(line)
+  table.insert(instructions, jmp)
+end
+
+local cur, steps = 1, 0
+while cur <= #instructions do
+  local jmp = instructions[cur]
+  instructions[cur] = jmp+1
+  cur = cur + jmp
+  steps = steps+1
+end
+
+print(steps)
