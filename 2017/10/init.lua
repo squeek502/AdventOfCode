d4efc4ddc96d6ed59072ac1a863f0b83df3830d6
Ryan Liptak <squeek502@hotmail.com>
2018-01-06 21:31:00 -0800

    2017 Day 10 part 2

--- 2017/10/init.lua
+++ 2017/10/init.lua
@@ -27,16 +27,70 @@ local function reverse(tbl, start, len)
   end
 end
 
-local function hash(list, lengths)
-  local curpos = 1
-  local skipsize = 0
-  for _, length in ipairs(lengths) do
-    reverse(list, curpos, length)
-    curpos = (curpos + length + skipsize) % #list
-    skipsize = skipsize + 1
+local function bxor(a,b)
+  local p,c=1,0
+  while a>0 and b>0 do
+    local ra,rb=a%2,b%2
+    if ra~=rb then c=c+p end
+    a,b,p=(a-ra)/2,(b-rb)/2,p*2
   end
-  return list[1] * list[2]
+  if a<b then a=b end
+  while a>0 do
+    local ra=a%2
+    if ra>0 then c=c+p end
+    a,p=(a-ra)/2,p*2
+  end
+  return c
 end
 
+local function hash(list, lengths, rounds)
+  if rounds == nil then rounds = 64 end
+  local curpos = 1
+  local skipsize = 0
+  local round = function()
+    for _, length in ipairs(lengths) do
+      reverse(list, curpos, length)
+      curpos = modIndex(curpos + length + skipsize, #list)
+      skipsize = skipsize + 1
+    end
+  end
+  for i=1,rounds do
+    round()
+  end
+  local dense = {}
+  for i=1,256,16 do
+    local n = list[i]
+    for j=i+1,i+15 do
+      n = bxor(n, list[j])
+    end
+    table.insert(dense, n)
+  end
+  local hash = ""
+  for _, n in ipairs(dense) do
+    hash = hash .. string.format("%02x", n)
+  end
+  return hash
+end
+
+-- Part 1
 local list = makeList(256)
-print(hash(list, lengths))
+hash(list, lengths, 1)
+print(list[1] * list[2])
+
+local function asciiToLengths(ascii)
+  local lengths = {}
+  for c in ascii:gmatch(".") do
+    local n = string.byte(c)
+    table.insert(lengths, n)
+  end
+  table.insert(lengths, 17)
+  table.insert(lengths, 31)
+  table.insert(lengths, 73)
+  table.insert(lengths, 47)
+  table.insert(lengths, 23)
+  return lengths
+end
+
+-- Part 2
+lengths = asciiToLengths(input)
+print(hash(makeList(256), lengths, 64))
