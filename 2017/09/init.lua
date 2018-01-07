82a086aeddadac02be413acd091bc6e0a489e0c4
Ryan Liptak <squeek502@hotmail.com>
2018-01-06 20:08:25 -0800

    2017 Day 9 part 1

---
+++ 2017/09/init.lua
@@ -0,0 +1,57 @@
+local input = io.open('input.txt'):read('*l')
+
+local Stack = {}
+Stack.__index = Stack
+Stack.__len = function(self) return #self.stack end
+
+function Stack.new()
+  local self = setmetatable({}, Stack)
+  self.stack = {}
+  return self
+end
+
+function Stack:push(v)
+  table.insert(self.stack, v)
+end
+
+function Stack:pop()
+  local v = table.remove(self.stack, #self.stack)
+  return v
+end
+
+function Stack:peek()
+  return self.stack[#self.stack]
+end
+
+function Stack:len()
+  return #self.stack
+end
+
+local groups = Stack.new()
+local garbage = false
+local ignored = false
+local score = 0
+for i=1,#input do
+  local char = input:sub(i,i)
+  if garbage then
+    if not ignored then
+      if char == "!" then
+        ignored = true
+      elseif char == ">" then
+        garbage = false
+      end
+    else
+      ignored = false
+    end
+  elseif char == "<" then
+    garbage = true
+  elseif char == "{" then
+    groups:push(char)
+    score = score + groups:len()
+  elseif char == "}" then
+    groups:pop()
+  end
+end
+assert(groups:len() == 0, "imbalanced groups")
+
+print(score)
