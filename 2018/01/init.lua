d8ff842d0ae7c115b06c10753df93417764880ed
Ryan Liptak <squeek502@hotmail.com>
2018-12-04 10:19:28 -0800

    2018 Day 1 part 2

--- 2018/01/init.lua
+++ 2018/01/init.lua
@@ -1,8 +1,24 @@
 local input = (function(f) local t={}; for l in assert(io.lines(f)) do t[#t+1] = l end; return t end)('input.txt')
 
 local sum = 0
-for i, v in ipairs(input) do
-  sum = sum + tonumber(v)
+local seen = {[sum] = true}
+local firstSum, firstDup
+
+while firstDup == nil do
+  for i, v in ipairs(input) do
+    sum = sum + tonumber(v)
+    if firstDup == nil and seen[sum] ~= nil then
+      firstDup = sum
+    end
+    seen[sum] = true
+  end
+  if firstSum == nil then
+    firstSum = sum
+  end
 end
 
-print(sum)
+-- Part 1
+print(firstSum)
+
+-- Part 2
+print(firstDup)
