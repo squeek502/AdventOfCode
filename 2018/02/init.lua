44dfe7a53f503d7a23f02c1d00b718d9374a0388
Ryan Liptak <squeek502@hotmail.com>
2018-12-04 10:53:11 -0800

    2018 Day 2 part 2

--- 2018/02/init.lua
+++ 2018/02/init.lua
@@ -1,5 +1,6 @@
 local input = (function(f) local t={}; for l in assert(io.lines(f)) do t[#t+1] = l end; return t end)('input.txt')
 
+-- Part 1
 local function letterCounts(str)
   local counts = {}
   for i=1,#str do
@@ -34,3 +35,40 @@ for _, id in ipairs(input) do
 end
 
 print(twos * threes)
+
+-- Part 2
+local function letterDifferences(a, b)
+  if #a ~= #b then return nil end
+  local diffs = 0
+  local lastDiffPos
+  for i=1,#a do
+    if a:sub(i,i) ~= b:sub(i,i) then
+      diffs = diffs + 1
+      lastDiffPos = i
+    end
+  end
+  return diffs, lastDiffPos
+end
+
+local first, second, diffpos
+for i, a in ipairs(input) do
+  for j, b in ipairs(input) do
+    if i ~= j then
+      local diffs, pos = letterDifferences(a, b)
+      if diffs == 1 then
+        first, second, diffpos = a, b, pos
+        break
+      end
+    end
+  end
+  if first ~= nil then
+    break
+  end
+end
+
+local function removePos(str, pos)
+  return str:sub(1, pos-1) .. str:sub(pos+1, #str)
+end
+
+assert(removePos(first, diffpos) == removePos(second, diffpos))
+print(removePos(first, diffpos))
