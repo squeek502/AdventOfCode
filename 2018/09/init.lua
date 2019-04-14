9c89b7fd89d5b19ad1bf172ad4d30d41421b13f5
Ryan Liptak <squeek502@hotmail.com>
2019-04-14 01:00:59 -0700

    2018 Day 9 part 2
    Changed to linked list implementation, naive array implementation was too slow for part 2

--- 2018/09/init.lua
+++ 2018/09/init.lua
@@ -3,30 +3,65 @@ local input = io.open('input.txt'):read('*l')
 local numPlayers, lastMarbleWorth = input:match("(%d+) players; last marble is worth (%d+) points")
 numPlayers, lastMarbleWorth = tonumber(numPlayers), tonumber(lastMarbleWorth)
 
-local scores = {}
-for i=1,numPlayers do
-  scores[i] = 0
-end
-
 local function modIndex(index, size)
   return ((index-1) % size) + 1
 end
 
-local marbles = {0}
-local currentMarble = 1
-local playerNum = 1
-for marble=1, lastMarbleWorth do
-  local insertIndex = modIndex(currentMarble+2, #marbles+1)
-  if insertIndex == 1 then insertIndex = insertIndex + 1 end
-  if marble % 23 == 0 then
-    currentMarble = modIndex(currentMarble - 7, #marbles)
-    local removed = table.remove(marbles, currentMarble)
-    scores[playerNum] = scores[playerNum] + marble + removed
-  else
-    table.insert(marbles, insertIndex, marble)
-    currentMarble = insertIndex
+local LinkedList = {}
+LinkedList.__index = LinkedList
+
+function LinkedList.new(v)
+  local self = setmetatable({}, LinkedList)
+  self.head = {value=v}
+  self.head.next = self.head
+  self.head.prev = self.head
+  return self
+end
+
+function LinkedList:insert(v)
+  local node = {value=v, prev=self.head, next=self.head.next}
+  self.head.next.prev = node
+  self.head.next = node
+  self.head = node
+end
+
+function LinkedList:get(deltaFromHead)
+  local node = self.head
+  for i=1,math.abs(deltaFromHead or 0) do
+    node = deltaFromHead > 0 and node.next or node.prev
   end
-  playerNum = modIndex(playerNum + 1, numPlayers)
+  return node
+end
+
+function LinkedList:moveHead(delta)
+  self.head = self:get(delta)
+end
+
+function LinkedList:remove(deltaFromHead)
+  local node = self:get(deltaFromHead)
+  node.prev.next, node.next.prev = node.next, node.prev
+  return node
+end
+
+local function computeScores(numPlayers, lastMarbleWorth)
+  local scores = {}
+  for i=1,numPlayers do
+    scores[i] = 0
+  end
+  local marbles = LinkedList.new(0)
+  for marble=1, lastMarbleWorth do
+    if marble % 23 == 0 then
+      marbles:moveHead(-7)
+      local removed = marbles:remove().value
+      local playerNum = modIndex(marble, numPlayers)
+      scores[playerNum] = scores[playerNum] + marble + removed
+      marbles:moveHead(1)
+    else
+      marbles:moveHead(1)
+      marbles:insert(marble)
+    end
+  end
+  return scores
 end
 
 local function highScore(scores)
@@ -40,5 +75,10 @@ local function highScore(scores)
   return highScore, player
 end
 
-local score = highScore(scores)
-print(score)
+-- Part 1
+local score1 = highScore(computeScores(numPlayers, lastMarbleWorth))
+print(score1)
+
+-- Part 2
+local score2 = highScore(computeScores(numPlayers, lastMarbleWorth*100))
+print(score2)
